49c969deca8cefc7eb7f62740404600f2d62ec13
ghaerdi <ghaerdi@ipisdosaj.edu.do>
2021-02-05 20:39:18 -0400

    refactor: main.js

--- main.js
+++ main.js
@@ -1,40 +1,56 @@
-const buttons = document.querySelectorAll(".answer-btn");
-const yesBtn = document.querySelector("#yes-btn");
-const noBtn = document.querySelector("#no-btn");
+function changeButtonPosition(button) {
+  let screenLimit = 40;
 
-buttons.forEach(btn =>
-  btn.addEventListener("click", () => alert("YES! I LOVE YOU! <3")));
+  for (let event of ["mouseenter", "mouseleave"]) {
+    button.addEventListener(event, function() {
+      toggleButton(button, event, "mouseenter");
 
-let screenLimit = 40;
-["mouseenter", "mouseleave"].forEach(event =>
-  noBtn.addEventListener(event, function(e) {
-    event === "mouseenter" ? changeToYesBtn(noBtn) : changeToNoBtn(noBtn);
+      if (event === "mouseenter") {
+        let positionX = randomNumber(screenLimit) * plusOrMinus();
+        let positionY = randomNumber(screenLimit) * plusOrMinus();
 
-    if (event === "mouseenter") {
-      let positionX = randomNumber(screenLimit) * plusOrMinus();
-      let positionY = randomNumber(screenLimit) * plusOrMinus();
+        button.style.transform = `translate(${positionX}vw, ${positionY}vh)`;
 
-      noBtn.style.transform = `translate(${positionX}vw, ${positionY}vh)`;
-      screenLimit &&= screenLimit - 0.1;
-    }
-  })
-);
+        screenLimit &&= screenLimit - 0.25;
+        console.log(screenLimit);
 
-["focus", "focusout"].forEach(event =>
-  noBtn.addEventListener(event, function(e) {
-    event === "focus" ? changeToYesBtn(noBtn) : changeToNoBtn(noBtn);
-  })
-);
-
-function changeToYesBtn(btn) {
-  btn.innerHTML = "Yes";
-  btn.style.background = "#6c9174";
+      }
+    })
+  }
 }
 
-function changeToNoBtn(btn) {
-  btn.innerHTML = "No";
-  btn.style.background = "#85515c";
+function focusButton(button) {
+  for (let event of ["focus", "focusout"]) {
+    button.addEventListener(event, () =>
+      toggleButton(button, event, "focus"));
+  }
+}
+
+function changeToYesButton(button) {
+  button.innerHTML = "Yes";
+  button.style.background = "#6c9174";
+}
+
+function changeToNoButton(button) {
+  button.innerHTML = "No";
+  button.style.background = "#85515c";
 }
 
 const randomNumber = limit => Math.floor((Math.random() * limit) + 0);
 const plusOrMinus = () => Math.round(Math.random()) * 2 - 1;
+const toggleButton = (button, actualEvent, desiredEvent) =>
+  actualEvent === desiredEvent ? changeToYesButton(button) : changeToNoButton(button);
+
+function main() {
+  const answerButtons = document.querySelectorAll(".answer-btn");
+  const yesButton = document.querySelector("#yes-btn");
+  const noButton = document.querySelector("#no-btn");
+
+  answerButtons.forEach(button =>
+    button.addEventListener("click", () => alert("YES! I LOVE YOU! <3")));
+
+  changeButtonPosition(noButton);
+  focusButton(noButton);
+}
+
+main();
